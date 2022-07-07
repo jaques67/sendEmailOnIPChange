2e2ab82af7edb5b015a96c5deadf92972f482771
Jaques Strydom <jaques.strydom@gmail.com>
2022-07-07 20:28:26 +0200

    Changes to program
    1. Encrypt message sent
    2. Use external source to determine IP address
    3. Use config file to contain configuration values

--- FindIpAddress.py
+++ FindIpAddress.py
@@ -1,51 +1,103 @@
+#!/usr/bin python
 import socket
 import smtplib
+import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from os.path import exists
+import urllib.request
+import configparser
+from cryptography.fernet import Fernet
 
 
-def get_old_ip_address():
-    with open("ip_address.ini", "r") as file:
+def get_old_ip_address(file_name):
+
+    if not exists(file_name):
+        return "0.0.0.0"
+    
+    with open(file_name, "r") as file:
         old_ip_address = file.read()
 
     return old_ip_address
 
 
-def get_current_ip_address():
-    # https://www.w3resource.com/python-exercises/python-basic-exercise-55.php
-    current_ip_address = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
-    if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
-    s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET,
-    socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+def get_current_ip_address(ip_check_service):
+
+    current_ip_address = urllib.request.urlopen(ip_check_service).read().decode('utf8')
 
     return current_ip_address
 
 
-def send_email(new_ip_address):
-    from_address = "gmail email address"
-    to_address = "recipient email address"
+def send_email(server_name, port_address, from_address, to_address, password, text):
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(server_name, port_address, context=context) as server:
+        server.login(from_address, password)
+        server.sendmail(from_address, to_address, text)
+    
+
+def create_email_message(new_ip_address, from_address, to_address):
+
     msg = MIMEMultipart()
     msg['From'] = from_address
     msg['To'] = to_address
     msg['Subject'] = "IP Address update"
      
-    body = "The new IP address is: " + new_ip_address
-    msg.attach(MIMEText(body, 'plain'))
-     
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(from_address, "place gmail password here")
+    body = encrypt_email_message(f"The new IP address is: {new_ip_address}")
+
+    msg.attach(MIMEText(str(body)))
     text = msg.as_string()
-    server.sendmail(from_address, to_address, text)
-    server.quit()
-    
-    
+
+    return text
+
+
+def encrypt_email_message(body):
+
+    fern_key = config['ENCRYPTION']['key']
+    cipher_suite = Fernet(fern_key)  # f
+
+    byte_string = bytes(body, 'utf-8')
+    ciphered_text = cipher_suite.encrypt(byte_string)  # token
+
+    return ciphered_text
+
+
+def write_current_ip_address(file_name):
+
+    with open(file_name, "w") as f:
+        f.write(ip_address)
+
+
+def parse_config_file():
+
+    # Read config
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    return config
+
+
 if __name__ == "__main__":
-    old_ip_address = get_old_ip_address()
-    ip_address = get_current_ip_address()
+    
+    config = parse_config_file()
+
+    file_name = config['IP_ADDRESS']['ip_config_file']
+
+    ip_check_service = config['IP_ADDRESS']['ip_check_service']
+    port_address = int(config['IP_ADDRESS']['port'])
+    server_name = config['IP_ADDRESS']['server_name']
+
+    from_address = config['EMAIL']['from_address']
+    to_address = config['EMAIL']['to_address']
+    password = config['EMAIL']['password']
+
+    old_ip_address = get_old_ip_address(file_name)
+    ip_address = get_current_ip_address(ip_check_service)
 
     if ip_address != old_ip_address:
-        with open("ip_address.ini", "w") as f:
-            f.write(ip_address)
-    
-        send_email(ip_address)
+        write_current_ip_address(file_name)
+        text = create_email_message(ip_address, from_address, to_address)
+        print('about to send email')
+        send_email(server_name, port_address, from_address, to_address, password, text)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sendEmailOnIPChange
-Python script that will send an email if the IP address is different from file.
+
+## Scenario
+In many 3rd world countries power is not stable.
+When a power outage occured the ISP will issue a new IP.
+This IP might be used for various different purposes.
+
+## Solution:
+When power is restored the python script will run and check whether the password has changed from the IP address stored on the file.
+If the IP address has changed, an email will be sent via GMail informing you that the IP address changed and wht the new IP address is.
